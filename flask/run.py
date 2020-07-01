87990f71cb77580ebb57c8a239c7cbd3e7b6ff1c
DavW <Davidpmwalsh@protonmail.com>
2020-07-01 13:44:53 +0000

    removed content from about page and populated with json data

--- flask/run.py
+++ flask/run.py
@@ -1,7 +1,7 @@
+
 import os
 import json
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
@@ -19,15 +19,14 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="Contact")
 
 
-@app.route("/careers")
+@app.route('/careers')
 def careers():
     return render_template("careers.html", page_title="Careers")
-
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
